50da398177b088524fd302013e0d560ee5e06d09
aaknitt <andyknitt@gmail.com>
2023-08-08 21:45:25 -0500

    first attempt at OpenMHz support

--- voxcall.py
+++ voxcall.py
@@ -15,6 +15,7 @@ import _thread
 import urllib3
 from shutil import copyfile
 import logging
+import json
 
 #logging.basicConfig(filename='log.txt',filemode='w',level=logger.debug)
 
@@ -135,6 +136,18 @@ try:
 	RDIO_tg_config = config.get('Section1','RDIO_tg')
 except:
 	RDIO_tg_config = ''
+try:
+	OpenMHz_APIkey_config = config.get('Section1','openmhz_api_key')
+except:
+	OpenMHz_APIkey_config = ''
+try:
+	OpenMHz_ShortName_config = config.get('Section1','openmhz_short_name')
+except:
+	OpenMHz_ShortName_config = ''
+try:
+	OpenMHz_tgid_config = config.get('Section1','openmhz_tgid')
+except:
+	OpenMHz_tgid_config = ''
 
 try:
 	root = Tk()
@@ -197,6 +210,12 @@ if root != '':
 	RDIO_tg.set(RDIO_tg_config)
 	RDIO_APIurl = StringVar()
 	RDIO_APIurl.set(RDIO_APIurl_config)
+	OpenMHz_APIkey = StringVar()
+	OpenMHz_APIkey.set(OpenMHz_APIkey_config)
+	OpenMHz_ShortName = StringVar()
+	OpenMHz_ShortName.set(OpenMHz_ShortName_config)
+	OpenMHz_tgid = StringVar()
+	OpenMHz_tgid.set(OpenMHz_tgid_config)
 	saveaudio = IntVar()
 	saveaudio.set(saveaudio_config)
 	in_channel = StringVar()
@@ -309,6 +328,54 @@ def upload_rdio(fname):
 	else:
 		logger.info("No rdio-scanner config detected, skipping upload to rdio-scanner API")
 
+def upload_openmhz(fname, start_time, duration):
+	if root != '':
+		api_key = OpenMHz_APIkey.get()
+		short_name = OpenMHz_ShortName.get()
+		freq = float(RadioFreq.get())*1e6
+		tgid = OpenMHz_tgid.get()
+	else:
+		api_key = OpenMHz_APIkey_config
+		short_name = OpenMHz_ShortName_config
+		freq = float(RadioFreq_config)*1e6
+		tgid = OpenMHz_tgid_config
+
+	if not api_key or not short_name or not tgid or not freq:
+		logger.error("OpenMHz API Key, tgid, freq, or Short Name not found.")
+		return False
+
+	source_list = []
+	
+	http = urllib3.PoolManager()
+	url=f"https://api.openmhz.com/{short_name}/upload"
+	f = open(fname,'rb')
+	audio_data = f.read()
+	f.close()
+	r = http.request(
+		'POST',
+		url,
+		fields={
+			'call': (os.path.basename(fname), audio_data, 'application/octet-stream'),
+			'freq': str(freq),
+			'error_count': str(0),
+			'spike_count': str(0),
+			'start_time': str(start_time),
+			'stop_time': str(start_time + duration),
+			'call_length': str(duration),
+			'talkgroup_num': str(tgid),
+			'emergency': str(0),
+			'api_key': api_key,
+			'source_list': json.dumps(source_list)
+		},
+		timeout=30)
+	if r.status != 200:
+		logger.debug("initial connect failed with status " + str(r.status))
+		logger.debug(r.data)
+	else:
+		logger.debug("upload to OpenMHz OK")
+	
+	return True
+
 def upload(fname,duration):
 	if (root != '' and BCFY_APIkey.get() != '') or (root == '' and BCFY_APIkey_config != ''):
 		if version.endswith('DEV'):
@@ -350,22 +417,25 @@ def upload(fname,duration):
 				logger.debug("error response from server: " + r.data.decode('utf-8'))
 	else:
 		logger.info("No BCFY config found, not attempting to upload there")
+
+		
+def cleanup_audio_files(fname):
 	if root != '':
 		saveit = saveaudio.get()
 	else:
 		saveit = saveaudio_config
-	if saveit == 0:
-		time.sleep(10)  #wait to make sure the other thread uploading to rdio-scanner has time to grab the file before deleting it
-		os.remove(fname)
-	else:
+	if saveit != 0:
 		try:
 			os.makedirs('./audiosave')
 		except OSError as e:
 			if e.errno != errno.EEXIST:
 				raise
-		copyfile(fname,'./audiosave/'+fname)
-		time.sleep(10)  #wait to make sure the other thread uploading to rdio-scanner has time to grab the file before deleting it
-		os.remove(fname)
+		logger.debug("Moving mp3 file for archiving")
+		copyfile(fname.replace('.wav','.mp3'),'./audiosave/'+fname.replace('.wav','.mp3'))
+	time.sleep(10)  #this is hacky - wait to make sure the upload threads have had time to grab the file before deleting it
+	logger.debug("Removing temporary audio files")
+	os.remove(fname.replace('.wav','.mp3'))
+	os.remove(fname.replace('.wav','.m4a'))
 
 def start():
 	global rec_debounce_counter
@@ -457,7 +527,8 @@ def start():
 				data = data
 			duration = len(data)/float(RATE)
 			#convert back to binary to write to WAV later
-			data = chararray.tostring(array(data))
+			#data = chararray.tostring(array(data))
+			data = chararray.tobytes(array(data))
 			# write data to WAVE file
 			fname = str(round(time.time())) + "-" + str(BCFY_SlotId.get()) + ".wav"
 			WAVE_OUTPUT_FILENAME = fname
@@ -476,7 +547,9 @@ def start():
 				except:
 					flags = 0
 				subprocess.call(["ffmpeg","-y","-i",fname,"-b:a",str(mp3_bitrate),"-ar","22050",fname.replace('.wav','.mp3')],creationflags=flags)
-				logger.debug("done converting to MP3 " + time.strftime('%H:%M:%S on %m/%d/%y'))				#use ffmpeg.exe 
+				logger.debug("done converting to MP3 " + time.strftime('%H:%M:%S on %m/%d/%y'))
+				subprocess.call(["ffmpeg","-y","-i",fname,"-b:a",str(mp3_bitrate),"-ar","22050",fname.replace('.wav','.m4a')],creationflags=flags)
+				logger.debug("done converting to M4A " + time.strftime('%H:%M:%S on %m/%d/%y'))
 				os.remove(fname)
 			except:
 				#exc_type, exc_value, exc_traceback = sys.exc_info()
@@ -484,6 +557,8 @@ def start():
 				logging.exception('Got exception on main handler')
 			_thread.start_new_thread(upload,(fname.replace('.wav','.mp3'),duration))
 			_thread.start_new_thread(upload_rdio,(fname.replace('.wav','.mp3'),))
+			_thread.start_new_thread(upload_openmhz,(fname.replace('.wav','.m4a'),start_time,duration))
+			_thread.start_new_thread(cleanup_audio_files,(fname,))
 			last_API_attempt = time.time()
 			logger.debug("duration: " + str(duration) + " sec")
 			logger.debug("waiting for audio " + time.strftime('%H:%M:%S on %m/%d/%y'))
@@ -512,6 +587,9 @@ def saveconfigdata():
 		config.set('Section1','RDIO_APIurl',RDIO_APIurl.get())
 		config.set('Section1','RDIO_system',RDIO_system.get())
 		config.set('Section1','RDIO_tg',RDIO_tg.get())
+		config.set('Section1','openmhz_api_key',OpenMHz_APIkey.get())
+		config.set('Section1','openmhz_short_name',OpenMHz_ShortName.get())
+		config.set('Section1','openmhz_tgid',OpenMHz_tgid.get())
 		config.write(cfgfile)
 		cfgfile.close()
 		root.destroy()
@@ -536,19 +614,26 @@ if root != '':
 	audiochannellist = OptionMenu(f,in_channel,"mono","left","right")
 	audiochannellist.config(width=20)
 	audiochannellist.grid(row = 5,column = 1,sticky=W)
-	Label(f,text='Broadcastify API Key:').grid(row=7,column=0,sticky = E)
-	BCFY_APIkey_Entry = Entry(f,width=40,textvariable = BCFY_APIkey)
-	BCFY_APIkey_Entry.grid(row = 7, column = 1,columnspan = 4,sticky=W)
-	Label(f,text='Broadcastify System ID:').grid(row=9,column=0,sticky = E)
-	BCFY_SystemId_Entry = Entry(f,width=20,validate='key',validatecommand=vcmd,textvariable = BCFY_SystemId)
-	BCFY_SystemId_Entry.grid(row = 9, column = 1,sticky=W)
-	Label(f,text='Broadcastify Slot ID:').grid(row=11,column=0,sticky = E)
-	BCFY_SlotId_Entry = Entry(f,width=20,validate='key',validatecommand=vcmd,textvariable = BCFY_SlotId)
-	BCFY_SlotId_Entry.grid(row = 11, column = 1,sticky=W)
-	Label(f,text='Radio Frequency (MHz):').grid(row=19,column=0,sticky = E)
+	Label(f,text='General Settings').grid(row=6,column=1,sticky = W)
+	Label(f,text='Radio Frequency (MHz):').grid(row=7,column=0,sticky = E)
+	
 	Freq_Entry = Entry(f,width=20,textvariable = RadioFreq)
-	Freq_Entry.grid(row = 19, column = 1,sticky=W)
+	Freq_Entry.grid(row = 7, column = 1,sticky=W)
+	Label(f,text='Save Audio Files:').grid(row=8,column=0,sticky=E)
+	Checkbutton(f,text = '',variable = saveaudio).grid(row = 8, column = 1,sticky=W)
+	
+	Label(f,text='Broadcastify Settings').grid(row=9,column=1,sticky = W)
+	Label(f,text='Broadcastify API Key:').grid(row=10,column=0,sticky = E)
+	BCFY_APIkey_Entry = Entry(f,width=40,textvariable = BCFY_APIkey)
+	BCFY_APIkey_Entry.grid(row = 10, column = 1,columnspan = 4,sticky=W)
+	Label(f,text='Broadcastify System ID:').grid(row=11,column=0,sticky = E)
+	BCFY_SystemId_Entry = Entry(f,width=20,validate='key',validatecommand=vcmd,textvariable = BCFY_SystemId)
+	BCFY_SystemId_Entry.grid(row = 11, column = 1,sticky=W)
+	Label(f,text='Broadcastify Slot ID:').grid(row=12,column=0,sticky = E)
+	BCFY_SlotId_Entry = Entry(f,width=20,validate='key',validatecommand=vcmd,textvariable = BCFY_SlotId)
+	BCFY_SlotId_Entry.grid(row = 12, column = 1,sticky=W)
 
+	Label(f,text='rdio-scanner settings').grid(row=19,column=1,sticky = W)
 	Label(f,text='rdio-scanner url:').grid(row=20,column=0,sticky = E)
 	RDIO_APIurl_Entry = Entry(f,width=40,textvariable = RDIO_APIurl)
 	RDIO_APIurl_Entry.grid(row = 20, column = 1,sticky=W)
@@ -562,9 +647,18 @@ if root != '':
 	RDIO_tg_Entry = Entry(f,width=20,validate='key',validatecommand=vcmd,textvariable = RDIO_tg)
 	RDIO_tg_Entry.grid(row = 23, column = 1,sticky=W)
 	
-	Label(f,text='Save Audio Files:').grid(row=24,column=0,sticky=E)
-	Checkbutton(f,text = '',variable = saveaudio).grid(row = 24, column = 1,sticky=W)
-	Button(f, text = "Save & Exit",command = saveconfigdata,width=20).grid(row = 25,column = 1,columnspan = 1,sticky=W)
+	Label(f,text='OpenMHz settings').grid(row=25,column=1,sticky = W)
+	Label(f,text='OpenMHz API Key:').grid(row=26,column=0,sticky = E)
+	OpenMhz_key_Entry = Entry(f,width=40,textvariable = OpenMHz_APIkey)
+	OpenMhz_key_Entry.grid(row = 26, column = 1,columnspan = 4,sticky=W)
+	Label(f,text='OpenMHz System ShortName').grid(row=27,column=0,sticky = E)
+	OpenMHz_ShortName_Entry = Entry(f,width=20, textvariable = OpenMHz_ShortName)
+	OpenMHz_ShortName_Entry.grid(row = 27, column = 1,sticky=W)
+	Label(f,text='OpenMHz TG/Channel ID:').grid(row=28,column=0,sticky = E)
+	OpenMHz_tgid_Entry = Entry(f,width=20,validate='key',validatecommand=vcmd,textvariable = OpenMHz_tgid)
+	OpenMHz_tgid_Entry.grid(row = 28, column = 1,sticky=W)
+	
+	Button(f, text = "Save & Exit",command = saveconfigdata,width=20).grid(row = 30,column = 1,columnspan = 1,sticky=W)
 	
 	squelchbar = Scale(f,from_ = 100, to = 0,length = 150,sliderlength = 8,showvalue = 0,variable = record_threshold,orient = 'vertical').grid(row = 17,rowspan=8,column = 7,columnspan = 1)
 	ttk.Progressbar(f,orient ='vertical',variable = barvar,length = 150).grid(row = 17,rowspan = 8,column = 8,columnspan = 1)
